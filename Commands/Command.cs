e7159648bb30fc25e09613e32fdaecd83ceb6802
leeyi45 <leeyi45@gmail.com>
2016-08-20 22:44:01 +0800

    Updated command definitions
    Minor tweaks to how each command function deals with message data

--- Commands/Command.cs
+++ Commands/Command.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Reflection;
-
+using System.Text;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace QuizBot
 {
-	/* The following classes are not my own work, but taken from the 
+  /* The following classes are not my own work, but taken from the 
 	 * werewolf for telegram bot
 	 * https://github.com/parabola949/Werewolf/
 	 */
-	internal static class UpdateHelper
+  internal static class UpdateHelper
 	{
 	  internal static bool IsGroupAdmin(Update update)
 	  {
@@ -85,10 +84,11 @@ namespace QuizBot
 	//Okay this is my work
 	public class Commands
 	{
+    #region Command Registration
     public static Dictionary<string, Tuple<CommandDelegate, Command>> AllCommands = 
       new Dictionary<string, Tuple<CommandDelegate, Command>>();
 
-		public delegate void CommandDelegate(Message msg, string originalMsg, string[] args);
+		public delegate void CommandDelegate(Message msg, string[] args);
 
 		public static void InitializeCommands()
 		{
@@ -115,7 +115,7 @@ namespace QuizBot
 			string[] args = cmd.Split(' ');
 			//Remove the @quiztestbot
 			if (args[0].Contains("@quiztestbot")) args[0] = args[0].Substring(0, 
-        args[0].Length - Program.Bot.GetMeAsync().Result.Username.Length - 1);
+        args[0].Length - GameData.BotUsername.Length - 1);
       //Program.ConsoleLog("The arg is " + args[0]);
       try
       {
@@ -143,7 +143,7 @@ namespace QuizBot
         }
         #endregion
 
-        AllCommands[args[0]].Item1(msg, cmd, args);
+        AllCommands[args[0]].Item1(msg, args);
       }
       catch (KeyNotFoundException)
       { //Program.ConsoleLog(e.Message); }
@@ -155,8 +155,159 @@ namespace QuizBot
 			Program.BotMessage(msg.Chat.Id, "NotAdminError", msg.From.FirstName);
 		}
 
-		#region Join Game Logic
-		public static void StartJoinGame(Message msg)
+    #region Commands
+    [Command(Trigger = "config", GroupAdminOnly = true)]
+    private static void Config(Message msg, string[] args)
+    {
+      //Send the config menu to the player
+      Program.BotMessage(msg.Chat.Id, "SentConfig", msg.From.GetName());
+      QuizBot.Config.SendInline(msg.From);
+    }
+
+    [Command(InGroupOnly = true, Trigger = "join")]
+    private static void Join(Message msg, string[] args)
+    {
+      var player = msg.From;
+      if (UpdateHelper.HasJoined(player) && GameData.GamePhase == GamePhase.Joining)
+      { //Joined already
+        Program.BotMessage("AlreadyJoin");
+      }
+      else if (GameData.GamePhase == GamePhase.Inactive)
+      { //Game is not running
+        Program.BotMessage("NoGameJoin");
+      }
+      else if (GameData.GamePhase == GamePhase.Running || GameData.GamePhase == GamePhase.Assigning)
+      { //Game is running
+        Program.BotMessage("GameRunningJoin");
+      }
+      else if (GameData.GamePhase == GamePhase.Joining)
+      { //Join the player thanks
+        Program.BotMessage(msg.Chat.Id, "PlayerJoin", player.Username, GameData.Joined.Count,
+          Settings.MinPlayers, Settings.MaxPlayers);
+        Program.BotMessage(msg.From.Id, "JoinGameSuccess", msg.Chat.Title);
+        GameData.Joined.Add(GameData.PlayerCount, player);
+      }
+    }
+
+    [Command(Trigger = "players")]
+    private static void Players(Message msg, string[] args)
+    {
+      if (GameData.GamePhase == GamePhase.Inactive) Program.BotMessage("NoGameJoin");
+      else
+      {
+        StringBuilder output = new StringBuilder("*Players*\n\n");
+        foreach (var each in GameData.Joined)
+        {
+          output.Append(each.Value.Username);
+          if (!each.Value.IsAlive) output.Append(": " + each.Value.role.ToString());
+          output.Append("\n");
+
+        }
+        Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
+      }
+    }
+
+    [Command(InGroupOnly = true, Trigger = "ping")]
+    private static void Ping(Message msg, string[] args)
+    {
+      var ts = DateTime.UtcNow - msg.Date;
+      var send = DateTime.UtcNow;
+      /*
+			var message = "PingInfo\n" + ts.ToString("mm\\:ss\\.ff") + "\n" +
+					System.Diagnostics..AvgCpuTime.ToString("F0") +
+					Program.MessageRxPerSecond.ToString("F0") + " MAX IN\n" +
+					Program.MessageTxPerSecond.ToString("F0") + "MAX OUT";*/
+      var message = "*PingInfo*\n" + "Time to receive ping message: " + ts.ToString("fff'.'ff") + "ms";
+      var result = Program.Bot.SendTextMessageAsync(msg.Chat.Id, message,
+        parseMode: ParseMode.Markdown).Result;
+      ts = DateTime.UtcNow - send;
+      Program.Bot.EditMessageTextAsync(msg.Chat.Id, result.MessageId, message + "\nTime to send ping message: " +
+        ts.ToString("fff'.'ff") + "ms", parseMode: ParseMode.Markdown);
+    }
+
+    [Command(InPrivateOnly = true, Trigger = "say")]
+    private static void Say(Message msg, string[] args)
+    {
+      //if (msg.Chat.Type == ChatType.Group) Program.BotMessage(msg.Chat.Id, "PrivateOnly");
+      string[] otherargs = new string[args.Length - 1];
+      Array.Copy(args, 1, otherargs, 0, args.Length - 1);
+      Program.Bot.SendTextMessageAsync(GameData.CurrentGroup, string.Join(" ", otherargs));
+      //catch { Program.BotMessage(msg.Chat.Id, "NoGameJoin"); }
+    }
+
+    [Command(Trigger = "start")]
+    private static void Start(Message msg, string[] args)
+    {
+      if (msg.Chat.Type == ChatType.Private)
+      {
+        //Program.BotMessage(msg.Chat.Id, "PleaseStartBot", msg.Chat.FirstName);
+        return;
+      }
+      if (GameData.GamePhase == GamePhase.Joining) Program.BotMessage("RunningGameStart");
+      // else 
+      else
+      {
+        GameData.CurrentGroup = msg.Chat.Id;
+        Program.ConsoleLog("Game started!");
+        StartJoinGame(msg);
+      }
+    }
+
+    [Command(Trigger = "roles")]
+    private static void Roles(Message msg, string[] args)
+    {
+      StringBuilder output = new StringBuilder("*" + Settings.CurrentRoleList + "*" + "\n\n");
+      foreach (var each in Settings.CurrentRoles)
+      {
+        output.Append(each.Key.Name + ", Count: " + each.Value.ToString() + "\n");
+      }
+      Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
+
+      /*
+			else if (args.Length == 2) // /role + rolename
+			{
+				string r = "";
+				try
+				{
+					r = args[1][0].ToString().ToUpper() + args[1].Substring(1, args[1].Length);
+
+					Program.Bot.SendTextMessageAsync(msg.Chat.Id, "*" + GameData.Roles[r].Name + "*\n" + GameData.Roles[r].description,
+						parseMode: ParseMode.Markdown);
+				}
+				catch (KeyNotFoundException)
+				{
+					Program.BotMessage(msg.Chat.Id, "RoleNotFound", r);
+				}
+				catch { }
+			}*/
+    }
+
+    [Command(Trigger = "listroles")]
+    private static void ListRoles(Message msg, string[] args)
+    {
+      StringBuilder output = new StringBuilder("<b>Currently Registered Roles:</b>\n\n");
+      foreach (var each in GameData.Roles)
+      {
+        output.AppendLine(each.Value.Name + ": <i>" + each.Value.description + "</i>");
+      }
+      Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Html);
+    }
+
+    [Command(Trigger = "version")]
+    private static void Version(Message msg, string[] args)
+    {
+      Assembly main = Assembly.GetExecutingAssembly();
+      var version = main.GetName().Version;
+      StringBuilder output = new StringBuilder("*Version information*\n\n");
+      output.AppendLine("Version: " + version.Major + "." + version.Minor);
+      output.AppendLine("Build: " + version.Build);
+      Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
+    }
+    #endregion
+    #endregion
+
+    #region Join Game Logic
+    public static void StartJoinGame(Message msg)
 		{
 			if (Settings.JoinTime < 60) Time.Tick += new EventHandler(TickHandler1);
 			else Time.Tick += new EventHandler(TickHandler2);
@@ -226,9 +377,9 @@ namespace QuizBot
         {
           player = noroles[randoms[i]];
           if(role[i].Key is Role) player.role = role[i].Key as Role;
-          else if (role[i].Key is Attribute)
+          else if (role[i].Key is Alignment)
           {
-            var attribute = role[i].Key as Attribute;
+            var attribute = role[i].Key as Alignment;
             var ranGen = new Random();
             if (attribute.Name == "Any")
             { //Assign any role to the player
@@ -277,162 +428,5 @@ namespace QuizBot
 			GameData.Joined = new Dictionary<int, Player>();
 		}
 
-		#region Commands
-		[Command(Trigger = "config", GroupAdminOnly = true)]
-		private static void Config(Message msg, string cmd, string[] args)
-		{
-			//Send the config menu to the player
-			Program.BotMessage(msg.Chat.Id, "SentConfig", msg.From.GetName());
-			QuizBot.Config.SendInline(msg.From);
-		}
-
-		[Command(InGroupOnly = true, Trigger = "join")]
-		private static void Join(Message msg, string cmd, string[] args)
-		{
-			var player = msg.From;
-			if (UpdateHelper.HasJoined(player) && GameData.GamePhase == GamePhase.Joining)
-			{ //Joined already
-				Program.BotMessage("AlreadyJoin");
-			}
-			else if (GameData.GamePhase == GamePhase.Inactive)
-			{ //Game is not running
-				Program.BotMessage("NoGameJoin");
-			}
-			else if (GameData.GamePhase == GamePhase.Running || GameData.GamePhase == GamePhase.Assigning)
-			{ //Game is running
-				Program.BotMessage("GameRunningJoin");
-			}
-			else if (GameData.GamePhase == GamePhase.Joining)
-			{ //Join the player thanks
-				Program.BotMessage(msg.Chat.Id, "PlayerJoin", player.Username, GameData.Joined.Count, 
-          Settings.MinPlayers, Settings.MaxPlayers);
-				GameData.Joined.Add(GameData.PlayerCount, player);
-			}
-		}
-
-		[Command(Trigger = "players")]
-		private static void Players(Message msg, string cmd, string[] args)
-		{
-			if (GameData.GamePhase == GamePhase.Inactive) Program.BotMessage("NoGameJoin");
-			else
-			{
-				StringBuilder output = new StringBuilder("*Players*\n\n");
-				foreach (var each in GameData.Joined)
-				{
-					output.Append(each.Value.Username);
-          if (!each.Value.IsAlive) output.Append(": " + each.Value.role.ToString());
-          output.Append("\n");
-
-        }
-				Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
-			}
-		}
-
-		[Command(InGroupOnly = true, Trigger = "ping")]
-		private static void Ping(Message msg, string cmd, string[] args)
-		{
-			var ts = DateTime.UtcNow - msg.Date;
-			var send = DateTime.UtcNow;
-			/*
-			var message = "PingInfo\n" + ts.ToString("mm\\:ss\\.ff") + "\n" +
-					System.Diagnostics..AvgCpuTime.ToString("F0") +
-					Program.MessageRxPerSecond.ToString("F0") + " MAX IN\n" +
-					Program.MessageTxPerSecond.ToString("F0") + "MAX OUT";*/
-			var message = "*PingInfo*\n" + "Time to receive ping message: " + ts.ToString("fff'.'ff") + "ms";
-			var result = Program.Bot.SendTextMessageAsync(msg.Chat.Id, message,
-				parseMode: ParseMode.Markdown).Result;
-			ts = DateTime.UtcNow - send;
-			Program.Bot.EditMessageTextAsync(msg.Chat.Id, result.MessageId, message + "\nTime to send ping message: " +
-				ts.ToString("fff'.'ff") + "ms", parseMode: ParseMode.Markdown);
-		}
-
-		[Command(InPrivateOnly = true, Trigger = "say")]
-		private static void Say(Message msg, string cmd, string[] args)
-    {
-      //if (msg.Chat.Type == ChatType.Group) Program.BotMessage(msg.Chat.Id, "PrivateOnly");
-      args[0] = "";
-			try
-			{
-				Program.Bot.SendTextMessageAsync(GameData.CurrentGroup,
-					cmd.Substring(3 + GameData.WeirdThing.Length));
-			}
-			catch (ArgumentOutOfRangeException)
-			{
-				Program.Bot.SendTextMessageAsync(GameData.CurrentGroup,
-					cmd.Substring(3));
-			}
-      Program.Bot.SendTextMessageAsync(GameData.CurrentGroup, string.Concat(args));
-				//catch { Program.BotMessage(msg.Chat.Id, "NoGameJoin"); }
-		}
-
-		[Command(Trigger = "start")]
-		private static void Start(Message msg, string cmd, string[] args)
-		{
-			if (msg.Chat.Type == ChatType.Private)
-			{
-				//Program.BotMessage(msg.Chat.Id, "PleaseStartBot", msg.Chat.FirstName);
-				return;
-			}
-      if (GameData.GamePhase == GamePhase.Joining) Program.BotMessage("RunningGameStart");
-     // else 
-      else
-      {
-        GameData.CurrentGroup = msg.Chat.Id;
-        Program.ConsoleLog("Game started!");
-        StartJoinGame(msg);
-      }
-		}
-
-		[Command(Trigger = "roles")]
-		private static void Roles(Message msg, string cmd, string[] args)
-		{
-			StringBuilder output = new StringBuilder("*" + Settings.CurrentRoleList + "*" + "\n\n");
-			foreach (var each in Settings.CurrentRoles)
-			{
-				output.Append(each.Key.Name + ", Count: " + each.Value.ToString() + "\n");
-			}
-			Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
-      
-      /*
-			else if (args.Length == 2) // /role + rolename
-			{
-				string r = "";
-				try
-				{
-					r = args[1][0].ToString().ToUpper() + args[1].Substring(1, args[1].Length);
-
-					Program.Bot.SendTextMessageAsync(msg.Chat.Id, "*" + GameData.Roles[r].Name + "*\n" + GameData.Roles[r].description,
-						parseMode: ParseMode.Markdown);
-				}
-				catch (KeyNotFoundException)
-				{
-					Program.BotMessage(msg.Chat.Id, "RoleNotFound", r);
-				}
-				catch { }
-			}*/
-		}
-
-    [Command(Trigger = "listroles")]
-    private static void ListRoles(Message msg, string cmd, string[] args)
-    {
-      StringBuilder output = new StringBuilder("<b>Currently Registered Roles:</b>\n\n");
-      foreach(var each in GameData.Roles)
-      {
-        output.AppendLine(each.Value.Name + ": <i>" + each.Value.description + "</i>");
-      }
-      Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Html);
-    }
-
-    [Command(Trigger = "version")]
-    private static void Version(Message msg, string cmd, string[] args)
-    {
-      Assembly main = Assembly.GetExecutingAssembly();
-      var version = main.GetName().Version;
-      StringBuilder output = new StringBuilder("*Version information*\n\n");
-      output.AppendLine("Version: " + version.Major + "." + version.Minor);
-      output.AppendLine("Build: " + version.Build);
-      Program.Bot.SendTextMessageAsync(msg.Chat.Id, output.ToString(), parseMode: ParseMode.Markdown);
-    }
-		#endregion
 	}
 }
