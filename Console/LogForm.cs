c4e7d9a7cd73d8e4057475ffe13455974c3b8bdf
leeyi45 <leeyi45@gmail.com>
2016-08-20 17:34:42 +0800

    Changed console command system
    Follows the new command structure, where through reflection the commands are added to a dictionary and then accessed from there at runtime

--- Console/LogForm.cs
+++ Console/LogForm.cs
@@ -6,227 +6,262 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+
+using Telegram.Bot.Types.Enums;
 
 //Event Data for the Console
 namespace QuizBot
 {
-	partial class LogForm : Form
-	{
-		public LogForm()
-		{
-			InitializeComponent();
-		}
+  partial class LogForm : Form
+  {
+    public LogForm()
+    {
+      InitializeComponent();
+      AddCommands();
+    }
 
-		private void CloseButton_Click(object sender, EventArgs e)
-		{
-			try { Program.Bot.StopReceiving(); }
-			catch { }
-			Application.Exit();
-		}
+    private void CloseButton_Click(object sender, EventArgs e)
+    {
+      try { Program.Bot.StopReceiving(); }
+      catch { }
+      Application.Exit();
+    }
 
-		private void CancelKey2(object sender, KeyPressEventArgs e)
-		{
-			e.Handled = true;
-		}
+    private void CancelKey2(object sender, KeyPressEventArgs e)
+    {
+      e.Handled = true;
+    }
 
-		private void CancelKey(object sender, KeyEventArgs e)
-		{
-			if (!e.Control) return;
-			switch (e.KeyCode)
-			{
-				case Keys.A:
-					{
-						logBox.SelectAll();
-						break;
-					}
-				case Keys.C:
-					{
-						try { Clipboard.SetText(logBox.SelectedText); }
-						catch { }
-						break;
-					}
-			}
-		}
+    private void CancelKey(object sender, KeyEventArgs e)
+    {
+      if (!e.Control) return;
+      switch (e.KeyCode)
+      {
+        case Keys.A:
+          {
+            logBox.SelectAll();
+            break;
+          }
+        case Keys.C:
+          {
+            try { Clipboard.SetText(logBox.SelectedText); }
+            catch { }
+            break;
+          }
+      }
+    }
 
-		private void TextBoxPress(object sender, KeyEventArgs e)
-		{
-			switch (e.KeyCode)
-			{
-				#region Escape
-				case Keys.Escape:
-					{
-						commandBox.Clear();
-						break;
-					}
-				#endregion
-				#region Enter
-				case Keys.Enter:
-					{
-						DebugMenu(commandBox.Text);
-						if (!string.IsNullOrWhiteSpace(commandBox.Text))
-						{
-							pastCommands.Add(pastCommands.Count + 1, commandBox.Text);
-						}
-						commandBox.Clear();
-						selectedCommand = 0;
-						break;
-					}
-				#endregion
-				#region Up
-				case Keys.Up:
-					{
-						if (pastCommands.Count == 0) return;
-						if (selectedCommand + 1 > pastCommands.Count)
-						{
-							commandBox.Clear();
-							selectedCommand = 0;
-						}
-						else
-						{
-							selectedCommand += 1;
-							commandBox.Text = pastCommands[selectedCommand];
-						}
-						break;
-					}
-				#endregion
-				#region Down
-				case Keys.Down:
-					{
-						if (pastCommands.Count == 0) return;
-						if (selectedCommand == 1)
-						{
-							commandBox.Clear();
-							selectedCommand = 0;
-						}
-						else if (selectedCommand == 0)
-						{
-							selectedCommand = pastCommands.Count;
-							commandBox.Text = pastCommands[selectedCommand];
-						}
-						else
-						{
-							selectedCommand -= 1;
-							commandBox.Text = pastCommands[selectedCommand];
-						}
-						break;
-					}
-				#endregion
-				#region A
-				//For some reason control a stopped working so I needed to reimplement this
-				case Keys.A: 
-					{
-						if (e.Control)
-						{
-							commandBox.SelectAll();
-						}
-						break;
-					}
-				#endregion
-			}
-		}
+    private void TextBoxPress(object sender, KeyEventArgs e)
+    {
+      switch (e.KeyCode)
+      {
+        #region Escape
+        case Keys.Escape:
+          {
+            commandBox.Clear();
+            break;
+          }
+        #endregion
+        #region Enter
+        case Keys.Enter:
+          {
+            DebugMenu(commandBox.Text);
+            if (!string.IsNullOrWhiteSpace(commandBox.Text))
+            {
+              pastCommands.Add(pastCommands.Count + 1, commandBox.Text);
+            }
+            commandBox.Clear();
+            selectedCommand = 0;
+            break;
+          }
+        #endregion
+        #region Up
+        case Keys.Up:
+          {
+            if (pastCommands.Count == 0) return;
+            if (selectedCommand + 1 > pastCommands.Count)
+            {
+              commandBox.Clear();
+              selectedCommand = 0;
+            }
+            else
+            {
+              selectedCommand += 1;
+              commandBox.Text = pastCommands[selectedCommand];
+            }
+            break;
+          }
+        #endregion
+        #region Down
+        case Keys.Down:
+          {
+            if (pastCommands.Count == 0) return;
+            if (selectedCommand == 1)
+            {
+              commandBox.Clear();
+              selectedCommand = 0;
+            }
+            else if (selectedCommand == 0)
+            {
+              selectedCommand = pastCommands.Count;
+              commandBox.Text = pastCommands[selectedCommand];
+            }
+            else
+            {
+              selectedCommand -= 1;
+              commandBox.Text = pastCommands[selectedCommand];
+            }
+            break;
+          }
+        #endregion
+        #region A
+        //For some reason control a stopped working so I needed to reimplement this
+        case Keys.A:
+          {
+            if (e.Control)
+            {
+              commandBox.SelectAll();
+            }
+            break;
+          }
+          #endregion
+      }
+    }
 
-		public void Log(string text, bool timestamp = true)
-		{
-			if (timestamp) logBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + text);
-			else logBox.AppendText(text);
-		}
+    public void Log(string text, bool timestamp = true)
+    {
+      if (timestamp) logBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + text);
+      else logBox.AppendText(text);
+    }
 
-		public void LogLine(string text, bool timestamp = true)
-		{
-			Log(text + "\n", timestamp);
-		}
+    public void LogLine(string text, bool timestamp = true)
+    {
+      Log(text + "\n", timestamp);
+    }
 
-		public void StartBot()
-		{
-			if (Program.Bot.IsReceiving) return;
-			Log("Starting...");
-			Thread.Sleep(600);
-			Program.Bot.StartReceiving();
-			LogLine("Bot started receving messages", false);
-			StatusLabel.Text = "Running";
-			StatusLabel.ForeColor = Color.ForestGreen;
-		}
+    private void StartButton_Click(object sender, EventArgs e)
+    {
+      StartBot();
+    }
 
-		public void StopBot()
-		{
-			if (!Program.Bot.IsReceiving) return;
-			Log("Stopping... ");
-			Thread.Sleep(600);
-			Program.Bot.StopReceiving();
-			LogLine("Bot stopped receving messages", false);
-			StatusLabel.Text = "Stopped";
-			StatusLabel.ForeColor = Color.Red;
-			return;
-		}
+    public void StartBot(bool yes = true)
+    {
+      if (Program.Bot.IsReceiving) return;
+      if(yes) Log("Starting...");
+      Thread.Sleep(600);
+      Program.Bot.StartReceiving();
+      if(yes) LogLine("Bot started receving messages", false);
+      StatusLabel.Text = "Running";
+      StatusLabel.ForeColor = Color.ForestGreen;
+    }
 
-		public async void DebugMenu(string input)
-		{
-			if (string.IsNullOrWhiteSpace(input)) return;
-			string[] args = input.Split(' ');
-			switch (args[0])
-			{
-				#region StartStop
-				case "stop":
-					{
-						StopBot();
-						return;
-					}
-				case "start":
-					{
-						StartBot();
-						return;
-					}
-				#endregion
-				case "say":
-					{
-						await Program.Bot.SendTextMessageAsync(Chats.WFPChat, input.Substring(4, input.Length-4));
-						LogLine("Message sent");
-						break;
-					}
-				case "xml":
-					{
-						GameData.InitializeRoles();
-						break;
-					}
-				case "roles":
-					{
-						foreach (var each in GameData.RoleLists["Default"])
-						{
-							Console.WriteLine(each.Key.Name);
-						}
-						Console.ReadLine();
-						break;
-					}
-				case "parseSay":
-					{
-						await Program.Bot.SendTextMessageAsync(Chats.WFPChat, input.Substring(9, input.Length - 9), 
-							parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
-						LogLine("Message sent");
-						break;
-					}
-				default:
-					{
-						LogLine("Unrecognised command: " + args[0]);
-						break;
-					}
-			}
-		}
+    public void StopBot(bool yes = true)
+    {
+      if (!Program.Bot.IsReceiving) return;
+      if(yes) Log("Stopping... ");
+      Thread.Sleep(600);
+      Program.Bot.StopReceiving();
+      if(yes) LogLine("Bot stopped receving messages", false);
+      StatusLabel.Text = "Stopped";
+      StatusLabel.ForeColor = Color.Red;
+      return;
+    }
 
-		private void StartButton_Click(object sender, EventArgs e)
-		{
-			StartBot();
-		}
+    private void StopButton_Click(object sender, EventArgs e)
+    {
+      StopBot();
+    }
 
-		private void StopButton_Click(object sender, EventArgs e)
-		{
-			StopBot();
-		}
+    private AutoCompleteStringCollection AddSuggestStrings()
+    {
+      var col = new AutoCompleteStringCollection();
+      col.AddRange(new string[] { "test" });
+      return col;
+    }
 
-		private AutoCompleteStringCollection AddSuggestStrings()
-		{
-			var col = new AutoCompleteStringCollection();
-			col.AddRange(new string[] { "test" });
-			return col;
-		}
-	}
+    #region Stuff for the console
+    private Dictionary<string, ConsoleCommand> ConsoleCommands;
+
+    private delegate string ConsoleCommand(string[] args);
+
+    private void AddCommands()
+    {
+      ConsoleCommands = new Dictionary<string, ConsoleCommand>();
+      foreach (var method in typeof(LogForm).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance))
+      {
+        Command attribute = method.GetCustomAttribute(typeof(Command)) as Command;
+        if (attribute != null)
+        {
+          ConsoleCommands.Add(attribute.Trigger, (ConsoleCommand)Delegate.CreateDelegate(
+            typeof(ConsoleCommand), this, method));
+        }
+      }
+    }
+
+    #region Console Commands
+    [Command(Trigger = "say")]
+    private string Say(string[] args)
+    {
+      string[] otherargs = new string[args.Length - 2];
+      Array.Copy(args, 2, otherargs, 0, args.Length - 2);
+      try
+      {
+        Program.Bot.SendTextMessageAsync(Chats.chats[args[1].ToLower()], string.Join(" ", otherargs));
+        return "Message sent";
+      }
+      catch (KeyNotFoundException) { return "Unknown chat: " + args[1]; }
+    }
+
+    [Command(Trigger = "parseSay")]
+    private string parseSay(string[] args)
+    {
+      string[] otherargs = new string[args.Length - 3];
+      Array.Copy(args, 3, otherargs, 0, args.Length - 3);
+      try
+      {
+        ParseMode mode;
+        if (!Enum.TryParse(args[2], out mode)) mode = ParseMode.Html;
+
+        Program.Bot.SendTextMessageAsync(Chats.chats[args[1].ToLower()],
+           string.Join(" ", otherargs), parseMode: mode);
+        return "Message sent";
+      }
+      catch (KeyNotFoundException) { return "Unknown chat: " + args[1]; }
+    }
+
+    [Command(Trigger = "start")]
+    private string Start(string[] args) { StartBot(false); return "Bot started receiving messages"; }
+
+    [Command(Trigger = "stop")]
+    private string Stop(string[] args) { StopBot(false); return "Bot stopped receiving messages"; }
+
+    [Command(Trigger = "reload")]
+    private string Reload(string[] args)
+    {
+      switch (args[1])
+      {
+        case "roles": { GameData.InitializeRoles(); break; }
+        case "messages": { goto case "msgs"; }
+        case "msgs": { GameData.InitializeMessages(); break; }
+        default: { throw new ArgumentException(args[1]); }
+      }
+      return args[1] + " reloaded";
+    }
+    #endregion
+
+    public void DebugMenu(string input)
+    {
+      if (string.IsNullOrWhiteSpace(input)) return;
+      input.ToLower();
+      string[] args = input.Split(' ');
+      try
+      {
+        LogLine(ConsoleCommands[args[0]](args));
+      }
+      catch (KeyNotFoundException) { LogLine("Unknown command: " + args[0]); }
+      catch (ArgumentException e) { LogLine("Unknow argument: " + e); }
+    }
+    #endregion
+  }
 }
